350544569ba198b5797c30d6890d28b6da1b0839
Venkatasubramaniyam <145216155+Venkatasubramaniyam@users.noreply.github.com>
2026-03-06 17:50:34 +0530

    Add files via upload

---
+++ combined.py
@@ -0,0 +1,55 @@
+class Combined():
+    def Subfields():
+            list=['Machine Learning', 'Neural Networks', 'Vision', 'Robotics', 'Speech Processing', 'Natural Language Processing']
+            print("Sub-fields in AI are:")
+            for Subfields in list:
+                print(Subfields)
+    
+    def OddEven(num):
+            print("Enter a number: 52452")
+            if ((num % 2) == 1):
+                print(num,"is odd number")
+            else:
+                print(num,"is Even number")
+    
+    def Elegible(gender,age):
+            print("Your Gender:Male")
+            print("Your Age:20")
+            if age>=21:
+                print("ELIGIBLE")
+            else:
+                print("NOT ELIGIBLE")
+    
+    def percentage():
+            Subject1= 98
+            Subject2= 87
+            Subject3= 95
+            Subject4= 95
+            Subject5= 93
+            print("Subject1=",Subject1)
+            print("Subject2=",Subject2)
+            print("Subject3=",Subject3)
+            print("Subject4=",Subject4)
+            print("Subject5=",Subject5)
+            total=Subject1+Subject2+Subject3+Subject4+Subject5
+            percent=total/5
+            print("Total :",total)
+            print("Percentage :",percent)
+    
+    def triangle():
+            height=32
+            breadth=34
+            print("Height:",height)
+            print("Breadth:",breadth)
+            formula=(height*breadth)/2
+            print("Area Formula: (Height*Breadth)/2")
+            print("Area of Triangle:",formula)
+            Height1=2
+            Height2=4
+            Breadth=4
+            print("Height1:",Height1)
+            print("Height2:",Height2)
+            print("Breadth:",Breadth)
+            pf=Height1+Height2+Breadth
+            print("Perimeter formula: Height1+Height2+Breadth")
+            print("Perimeter of Triangle: ",pf)
